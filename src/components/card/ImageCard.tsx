207c66cc839aaff1cefebc500ec0eee6aed1d7b8
Ldrex1 <damzealwini7@gmail.com>
2025-01-28 03:31:56 +0100

    new file: image card component

---
+++ src/components/card/ImageCard.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+export type ImageCardProps = {
+  src: string;
+  styTail?: string;
+  alt: string;
+  width?: string;
+  aspect?: `aspect-[${number}] md:aspect-[${number}] lg:aspect-[${number}]`;
+  rounded?: boolean;
+};
+function ImageCard({
+  src,
+  alt,
+  width,
+  aspect,
+  styTail,
+  rounded,
+}: ImageCardProps): ReactNode {
+  const className = `relative lg:w-full ${
+    aspect || "aspect-[0.968] md:aspect-[1]"
+  } overflow-hidden ${rounded && "rounded-lg"} ${width} ${styTail}`;
+  return (
+    <div className={className}>
+      <Image
+        className="object-cover object-center"
+        src={src}
+        alt={alt}
+        fill
+        // placeholder="blur"
+        // blurDataURL={""}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      />
+    </div>
+  );
+}
+
+export default ImageCard;
