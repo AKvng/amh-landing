89197c1e60d33a5032c48dc999b9a2491f5b471d
Ldrex1 <damzealwini7@gmail.com>
2025-02-22 05:19:54 +0100

    new file: navigation library

---
+++ src/lib/nav.ts
@@ -0,0 +1,13 @@
+export const landingNavigation = [
+  { name: "features", href: "#features" },
+  { name: "pricing", href: "#pricing" },
+  { name: "about us", href: "#about" },
+  { name: "dashboard", href: "#dashboard" },
+  { name: "contact", href: "#contact" },
+];
+
+export const contact = [
+  { name: "phone", content: "123456789" },
+  { name: "email", content: "info@example.com" },
+  { name: "address", content: "Lagos" },
+];
