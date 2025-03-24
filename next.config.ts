d3eab8c49d80cfcfca8b379199dc260612e60326
Ldrex1 <damzealwini7@gmail.com>
2025-03-24 01:35:44 +0100

    update; static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+
+  // Add your custom webpack configuration here
+  //...
 };
 
 export default nextConfig;
