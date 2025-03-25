60a85eb35c34b4dc10e245b42de2a712f1520520
Ldrex1 <damzealwini7@gmail.com>
2025-03-25 12:42:49 +0100

    update: setting images as unoptimized

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
-// https://d8mbkzpwhwsvw.cloudfront.net/_next/image?url=%2Fblue-background.jpg&w=640&q=75
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  remotePatterns: [
-    {
-      urlPattern: "https://d8mbkzpwhwsvw.cloudfront.net/",
-      // Add more properties if needed
-    },
-  ],
+  images: {
+    unoptimized: true,
+  },
 
   // Add your custom webpack configuration here
   //...
